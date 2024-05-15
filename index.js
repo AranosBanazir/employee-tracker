39e85845cce316b1220fdef45f7d51e5401c77b3
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-05-15 17:09:30 -0500

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+const pg    = require('pg')
+const colors = require('colors')
+
+//return to this menu after processing questions
+async function promptMenu(){
+const menuQuestions = [
+    {
+        "name": "",
+        "type": "",
+        "message": "",
+        "prefix": "",
+    }
+]
+
+
+}
