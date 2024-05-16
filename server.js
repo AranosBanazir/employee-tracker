ab52ac0a05f139155d7c7da42b686ffe4dff96e3
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-05-16 13:23:37 -0500

    added ASCII headers and separate menus for each section

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const inquirer = require("inquirer");
 const { Pool } = require("pg");
 const colors = require("colors");
-const { up } = require("inquirer/lib/utils/readline");
 require("dotenv").config();
-let departmentList;
-let rolesList;
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,42 +9,107 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
+require("dotenv").config();
+let departmentList;
+let rolesList;
+
+const departmentASCII = `
+
+ ______                                                       
+(______)                           _                      _   
+ _     _ _____ ____  _____  ____ _| |_ ____  _____ ____ _| |_ 
+| |   | | ___ |  _ \\(____ |/ ___|_   _)    \\| ___ |  _ (_   _)
+| |__/ /| ____| |_| / ___ | |     | |_| | | | ____| | | || |_ 
+|_____/ |_____)  __/\\_____|_|      \\__)_|_|_|_____)_| |_| \\__)
+             |_|                                             
+      _______                                                
+     (_______)                                               
+      _  _  _ _____ ____  _____  ____ _____  ____            
+     | ||_|| (____ |  _ \\(____ |/ _  | ___ |/ ___)           
+     | |   | / ___ | | | / ___ ( (_| | ____| |               
+     |_|   |_\\_____|_| |_\\_____|\\___ |_____)_|               
+                               (_____|     
+
+`.green;
+
+const rolesASCII = `
+ ______       _                      
+(_____ \\     | |                     
+ _____) )___ | | _____  ___          
+|  __  // _ \\| || ___ |/___)         
+| |  \\ \\ |_| | || ____|___ |         
+|_|   |_\\___/ \\_)_____|___/          
+                                    
+ _______                                     
+(_______)                                    
+ _  _  _ _____ ____  _____  ____ _____  ____ 
+| ||_|| (____ |  _ \\(____ |/ _  | ___ |/ ___)
+| |   | / ___ | | | / ___ ( (_| | ____| |    
+|_|   |_\\_____|_| |_\\_____|\\___ |_____)_|    
+                 (_____|          
+                     
+                     `.green;
+
+const employeeASCII = `
+         _______             _                         
+        (_______)           | |                        
+         _____   ____  ____ | | ___  _   _ _____ _____ 
+        |  ___) |    \\|  _ \\| |/ _ \\| | | | ___ | ___ |
+        | |_____| | | | |_| | | |_| | |_| | ____| ____|
+        |_______)_|_|_|  __/ \\_)___/ \\__  |_____)_____)
+                    |_|           (____/             
+         _______                                     
+        (_______)                                    
+         _  _  _ _____ ____  _____  ____ _____  ____ 
+        | ||_|| (____ |  _ \\(____ |/ _  | ___ |/ ___)
+        | |   | / ___ | | | / ___ ( (_| | ____| |    
+        |_|   |_\\_____|_| |_\\_____|\\___ |_____)_|  
+
+`.green;
+
 //return to this menu after processing questions
-async function promptMenu() {
+function promptMenu() {
   inquirer
     .prompt({
       message: "What would you like to do?",
       name: "action",
       prefix: "",
       type: "list",
-      choices: [
-        "view all departments",
-        "view all roles",
-        "view all employees",
-        "add a department",
-        "add a role",
-        "add an employee",
-        "update an employee role",
-      ],
+      choices: ["Manage Departments", "Manage Employees", "Manage Roles"],
     })
     .then(handleChoice);
 }
 
 function handleChoice(a) {
-  if (a.action === "view all departments") {
+  if (a.action === "View all departments") {
     viewDepartments();
-  } else if (a.action === "view all roles") {
+  } else if (a.action === "View all roles") {
     viewRoles();
-  } else if (a.action === "view all employees") {
+  } else if (a.action === "View all employees") {
     viewEmployees();
-  } else if (a.action === "add a department") {
+  } else if (a.action === "Add a department") {
     addDepartment();
-  } else if (a.action === "add a role") {
+  } else if (a.action === "Add a role") {
     addRole();
-  } else if (a.action === "add an employee") {
+  } else if (a.action === "Add an employee") {
     addEmployee();
-  } else if (a.action === "update an employee role") {
+  } else if (a.action === "Update an employee role") {
     updateEmployeeRole();
+  } else if (a.action === "Manage Departments") {
+    manageDepartments();
+  } else if (a.action === "Manage Roles") {
+    manageRoles();
+  } else if (a.action === "Manage Employees") {
+    manageEmployees();
+  } else if (a.action === "Delete a department") {
+    deleteDepartment();
+  } else if (a.action === "Delete a role") {
+    deleteRole();
+  } else if (a.action === "Delete an employee") {
+    deleteEmployee();
+  } else if (a.action === "Return to main menu") {
+    console.clear();
+    promptMenu();
   }
 }
 
@@ -55,30 +117,12 @@ const viewDepartments = async () => {
   const result = await pool.query(
     `SELECT d.id, d.name AS "Department" FROM departments AS d;`
   );
+  console.log(departmentASCII);
   console.table(result.rows);
-  promptMenu();
+  manageDepartments();
 };
 
-const viewEmployees = async () => {
-  const result =
-    await pool.query(`Select e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", 
-  r.salary AS "Salary", d.name AS "Department" FROM employees AS e
-  JOIN roles AS r ON e.role_id = r.id
-  JOIN departments AS d ON r.id = d.id;`);
-
-  console.table(result.rows);
-  promptMenu();
-};
-
-const viewRoles = async () => {
-  const result =
-    await pool.query(`Select r.title AS "Title", d.name AS "Department" FROM roles AS r
-    JOIN departments AS d ON r.id = d.id;`);
-  console.table(result.rows);
-  promptMenu();
-};
-
-const addDepartment = async () => {
+const addDepartment = () => {
   inquirer
     .prompt({
       name: "department",
@@ -89,12 +133,36 @@ const addDepartment = async () => {
     .then(async (a) => {
       await pool.query(
         `INSERT INTO departments (name)
-            VALUES ($1)`,
+                VALUES ($1)`,
         [a.department]
       );
-      console.log(`Added ${a.department} to the database.`.green);
-      promptMenu();
+      console.log(`Added ${a.department} to the database.`);
+      manageDepartments();
     });
+};
+
+const deleteDepartment = async () => {
+  await getDepartments();
+  inquirer.prompt({
+    name: "department",
+    type: "list",
+    prefix: "",
+    choices: departmentList,
+    message: "Which department would you like to delete?",
+  });
+};
+
+const viewEmployees = async () => {
+  const result =
+    await pool.query(`Select e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS "Title", 
+    r.salary AS "Salary", d.name AS "Department" FROM employees AS e
+    JOIN roles AS r ON e.role_id = r.id
+    JOIN departments AS d ON r.id = d.id;`);
+
+  console.clear();
+  console.log(employeeASCII);
+  console.table(result.rows);
+  manageEmployees();
 };
 
 const addEmployee = async () => {
@@ -126,35 +194,38 @@ const addEmployee = async () => {
     .then(async (a) => {
       await pool.query(
         `
-      INSERT INTO employees (first_name, last_name, role_id, manager_id)
-      VALUES 
-      ($1, $2, (SELECT roles.id from roles WHERE roles.title = $3), '1');`,
+        INSERT INTO employees (first_name, last_name, role_id, manager_id)
+        VALUES 
+        ($1, $2, (SELECT roles.id from roles WHERE roles.title = $3), '1');`,
         [a.first_name, a.last_name, a.role]
       );
       console.log(
-        `${a.first_name.brightYellow} ${a.last_name.brightYellow} has been ` +
+        `${a.first_name} ${a.last_name} has been ` +
           "created".brightGreen +
-          ` and given the ${a.role.brightYellow} role.`
+          ` and given the ${a.role} role.`
       );
-      promptMenu();
+      manageEmployees();
     });
 };
 
-const addRole = async () => {};
-
 const updateEmployeeRole = async () => {};
-
-const updateManagers = async () => {};
-
-const deleteDepartment = async () => {};
-
-const deleteRole = async () => {};
-
 const deleteEmployee = async () => {};
-
 const viewEmployeesByManager = async () => {};
-
+const updateManagers = async () => {};
 const viewEmployeesByDepartment = async () => {};
+
+const viewRoles = async () => {
+  const result =
+    await pool.query(`Select r.title AS "Title", d.name AS "Department" FROM roles AS r
+        JOIN departments AS d ON r.id = d.id;`);
+  console.clear();
+  console.log(rolesASCII);
+  console.table(result.rows);
+  manageRoles();
+};
+
+const addRole = async () => {};
+const deleteRole = async () => {};
 
 const getDepartments = async () => {
   const result = await pool.query("SELECT d.name FROM departments AS d;");
@@ -168,9 +239,56 @@ const getRoles = async () => {
   rolesList = result.rows.map((role) => role.title);
 };
 
+const manageRoles = async () => {
+  inquirer
+    .prompt({
+      message: "\nWhat would you like to do?",
+      name: "action",
+      prefix: "",
+      type: "list",
+      choices: [
+        "View all roles",
+        "Add a role",
+        "Delete a role",
+        "Return to main menu",
+      ],
+    })
+    .then(handleChoice);
+};
+
+const manageDepartments = async () => {
+  inquirer
+    .prompt({
+      message: "\nWhat would you like to do?",
+      name: "action",
+      prefix: "",
+      type: "list",
+      choices: [
+        "View all departments",
+        "Add a department",
+        "Delete a department",
+        "Return to main menu",
+      ],
+    })
+    .then(handleChoice);
+};
+
+const manageEmployees = async () => {
+  inquirer
+    .prompt({
+      message: "What would you like to do?",
+      name: "action",
+      prefix: "",
+      type: "list",
+      choices: [
+        "View all employees",
+        "Add an employee",
+        "Delete an employee",
+        "Return to main menu",
+      ],
+    })
+    .then(handleChoice);
+};
+
 promptMenu();
 
-// const result =
-// await pool.query(``);
-// console.table(result.rows);
-// promptMenu();
