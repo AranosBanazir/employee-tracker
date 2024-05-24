8f51251a6eea36a052fa76f9a8986fd72b1c0af2
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-05-23 22:03:56 -0500

    added readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,6 @@ VALUES
 INSERT INTO roles (title, salary, department)
 VALUES 
        ('Janitor', '33000', '6'),
-       ('Manager', '50000', '2'),
        ('Sales Lead', '30000', '2'),
        ('Software Engineer', '90000', '4'),
        ('Software Engineering Lead', '200000', '4'),
@@ -22,4 +21,4 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id, manager)
     VALUES 
         ('Caleb', 'Saiia', '4', '1', true),
         ('John', 'Doe', '2', null, false),
-        ('Tom', 'Allen', '3', null, false);
+        ('Tina', 'Allen', '3', null, false);
